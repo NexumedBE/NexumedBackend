164e30947e0d3eceb9c2505eef266b5f56feb357
cdnbelgium <jscharlach@hotmail.com>
2025-02-27 10:45:50 +0100

    pj change

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import connectDB from './config/db';
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Backend running on http://localhost:${PORT}`);
